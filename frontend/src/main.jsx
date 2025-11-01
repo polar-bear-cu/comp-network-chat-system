33425313d27a8241a424d9df571cc12901c1aae9
BadLuckZ <thanagorn8802@gmail.com>
2025-11-01 23:49:46 +0700

    feat: middleware

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,18 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import LandingPage from "./page/LandingPage.jsx";
+import LoginPage from "./page/LoginPage";
+import SignupPage from "./page/SignupPage";
+import ChatPage from "./page/ChatPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
   },
+  { path: "/signup", element: <SignupPage /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "/chat", element: <ChatPage /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
