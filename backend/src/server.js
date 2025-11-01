71fe05c5be75323939a5611b0ee9e80345092c28
BadLuckZ <thanagorn8802@gmail.com>
2025-11-01 21:37:08 +0700

    chore: connect mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -24,4 +25,5 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
