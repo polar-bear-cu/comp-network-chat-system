cb7aef7a7d15e18f29a4149e5319f97777c533e8
BadLuckZ <thanagorn8802@gmail.com>
2025-11-01 21:12:19 +0700

    chore: deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
