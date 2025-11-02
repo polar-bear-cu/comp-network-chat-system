b5eff3bcd8c11df35dedf5a693470c56b511df13
BadLuckZ <thanagorn8802@gmail.com>
2025-11-02 12:08:01 +0700

    feat: get all contacts api

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllContacts } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send Message!");
-});
+router.use(protectRoute);
+
+router.get("/contacts", getAllContacts);
 
 export default router;
