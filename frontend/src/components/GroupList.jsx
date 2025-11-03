abe6a0320a05c08c69031cb53ca616b76825a5dd
PGS-8 <155666692+PGS-8@users.noreply.github.com>
2025-11-03 13:38:37 +0700

    feat: check icon as member of the group (#8)
    * fix: try using socket to do joining
    
    * fix: wrong vars
    
    * feat: show who is owner
    
    * fix: wrong owner id
    
    * feat: try connecting socket to implement real-time group messages
    
    * fix: rename function
    
    * feat: try using socket to get real-time contact
    
    * fix: reverse users
    
    * fix: reverse again
    
    * fix: reverse again
    
    * fix: reverse ...
    
    * fix: contact order
    
    * fix: reverse...
    
    * fix: sort
    
    * fix: sort
    
    * feat: check when you are in the group
    
    ---------
    
    Co-authored-by: BadLuckZ <thanagorn8802@gmail.com>

--- frontend/src/components/GroupList.jsx
+++ frontend/src/components/GroupList.jsx
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { useGroupStore } from "@/store/useGroupStore";
 import NoGroupFound from "./NoGroupFound";
 import UsersLoadingSkeleton from "./UserLoadingSkeleton";
-import { Users } from "lucide-react";
+import { Check, Users } from "lucide-react";
+import { useAuthStore } from "@/store/useAuthStore";
 
 const GroupList = () => {
+  const { authUser } = useAuthStore();
   const { getAllGroups, allGroups, setSelectedGroup, isGroupsLoading } =
     useGroupStore();
 
@@ -17,29 +19,42 @@ const GroupList = () => {
   if (!Array.isArray(allGroups) || allGroups.length === 0)
     return <NoGroupFound />;
 
+  const isMember = (group) => {
+    return group.members?.some((member) => member._id === authUser?._id);
+  };
+
   return (
     <>
-      {[...allGroups].reverse().map((group) => (
-        <div
-          key={group._id}
-          className="p-4 rounded-lg cursor-pointer bg-muted/30 border border-border hover:bg-muted/50 transition-colors"
-          onClick={() => setSelectedGroup(group)}
-        >
-          <div className="flex items-center gap-3">
-            <div className="w-12 h-12 rounded-full relative bg-muted flex items-center justify-center border border-border">
-              <Users className="w-8 h-8 text-primary" />
-            </div>
-            <div className="flex flex-col">
-              <h4 className="font-medium truncate text-foreground/90">
-                {group.name}
-              </h4>
-              <p className="text-sm truncate text-foreground/60">
-                By {group.owner.username}
-              </p>
+      {[...allGroups].reverse().map((group) => {
+        const memberStatus = isMember(group);
+
+        return (
+          <div
+            key={group._id}
+            className="p-4 rounded-lg cursor-pointer bg-muted/30 border border-border hover:bg-muted/50 transition-colors"
+            onClick={() => setSelectedGroup(group)}
+          >
+            <div className="flex items-center gap-3">
+              <div className="w-12 h-12 rounded-full relative bg-muted flex items-center justify-center border border-border">
+                <Users className="w-8 h-8 text-primary" />
+              </div>
+              <div className="flex flex-col">
+                <div className="flex items-center gap-1">
+                  <h4 className="font-medium truncate text-foreground/90">
+                    {group.name}
+                  </h4>
+                  {memberStatus && (
+                    <Check className="w-4 h-4 text-primary shrink-0" />
+                  )}
+                </div>
+                <p className="text-sm truncate text-foreground/60">
+                  By {group.owner.username}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
